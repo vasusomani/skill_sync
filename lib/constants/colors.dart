0f4df8eecdfdd4d9158dd4cdcad51a8ff080a5c4
Vasu Somani <vasusomani2004@gmail.com>
2024-03-10 01:46:48 +0530

    Added project architecture

---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static final backgroundColor = Color(0xFF656565);
+  static final primaryColor = Color(0xFFFFBC0E);
+  static final textFieldsColor = Color(0xFFFFFFFF);
+  //Add more colors here
+}
